b2354d3345b8aed93260095f81064d27dab1b5d9
Reverad <sektumpro@gmail.com>
2026-05-19 16:14:23 +0300

    fixes

--- taxi/urls.py
+++ taxi/urls.py
@@ -56,7 +56,7 @@ urlpatterns = [
         name="driver-delete"
     ),
     path(
-        "drivers/<int:pk>/update",
+        "drivers/<int:pk>/update/",
         DriverUpdateView.as_view(),
         name="driver-update"
     ),
